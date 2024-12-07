f2dc5a8aa63e81c9ec231e803446e5e4f046b364
Emanuel Kuhn <kuhn.emanuel@gmail.com>
2024-12-07 17:41:19 +0100

    Redo day 6

--- AoC2024/Day6.cs
+++ AoC2024/Day6.cs
@@ -1,130 +1,146 @@
 ﻿using System.Collections.Frozen;
-using System.Collections.Immutable;
-using System.Diagnostics;
 
 namespace AoC2024;
 
 public class Day6() : AoCDay(day: 6, hasTwoInputs: false)
 {
-    [Flags]
-    private enum Orientation
-    {
-        Up, 
-        Down, 
-        Left, 
-        Right
-    }
+    record struct Guard(Vec2 Position, char Direction);
     
-    private Orientation ParseDirection(char c)
+    class World
     {
-        return c switch
-        {
-            '^' => Orientation.Up,
-            'v' => Orientation.Down,
-            '<' => Orientation.Left,
-            '>' => Orientation.Right,
-            _ => throw new ArgumentOutOfRangeException(nameof(c), c, null)
-        };
-    }
-    
-    private static Orientation TurnRight(Orientation direction)
-    {
-        return direction switch
-        {
-            Orientation.Up => Orientation.Right,
-            Orientation.Down => Orientation.Left,
-            Orientation.Left => Orientation.Up,
-            Orientation.Right => Orientation.Down,
-            _ => throw new ArgumentOutOfRangeException(nameof(direction), direction, null)
-        };
-    }
+        public Guard Guard { get; private set; }
 
-    private static ImmutableArray<char> _guardSymbols = ['^', 'v', '<', '>'];
+        private FrozenSet<Vec2> Obstructions { get; }
+        
+        public HashSet<Guard> Visited { get; }
+        
+        Vec2 Size { get; }
 
-    private record Vec2(int R, int C)
-    {
-        public Vec2 Move(Vec2 v)
+        private World(Guard guard, FrozenSet<Vec2> obstructions, HashSet<Guard> visited, Vec2 size)
         {
-            return new Vec2(R + v.R, C + v.C);
+            Guard = guard;
+            Obstructions = obstructions;
+            Visited = visited;
+            Size = size;
+        }
+
+        public World(Guard guard, FrozenSet<Vec2> obstructions, Vec2 size) : this(guard, obstructions, [guard], size) { }
+
+        private (bool isValid, bool looped) Step()
+        {
+            if (NextGuard() is not { } nextGuard) return (false, false);
+
+            var looped = Visited.Contains(nextGuard);
+            
+            Visited.Add(nextGuard);
+            Guard = nextGuard;
+
+            return (true, looped);
         }
         
-        
-        
-        public Vec2 MoveUnit(Orientation direction)
+        public bool WillLoop()
         {
-            return Move(AsUnit(direction));
-        }
-        
-        public static Vec2 AsUnit(Orientation direction)
-        {
-            return direction switch
-            { 
-                Orientation.Up => new Vec2(-1, 0),
-                Orientation.Down => new Vec2(1, 0),
-                Orientation.Left => new Vec2(0, -1),
-                Orientation.Right => new Vec2(0, 1),
-                _ => throw new ArgumentOutOfRangeException(nameof(direction), direction, null)
-            };
-        }
-    }
-    
-    private interface IGridItem
-    {
-        Vec2 Position { get; }
-    }
-
-    private class Guard(Vec2 position, Orientation direction) : IGridItem
-    {
-        public Vec2 Position { get; private set; } = position;
-        public Orientation Direction { get; private set; } = direction;
-
-        public void Move(FrozenSet<Vec2> obstructions)
-        {
-            var maybeNewPosition = Position.MoveUnit(Direction);
-
-            while (obstructions.Contains(maybeNewPosition))
+            while (Step() is { isValid: true } result)
             {
-                Direction = TurnRight(Direction);
-                maybeNewPosition = Position.MoveUnit(Direction);
+                if (result.looped) return true;
             }
 
-            Position = maybeNewPosition;
+            return false;
+        }
+        
+        public World Cloned() => new(Guard, Obstructions, Visited.AsEnumerable().ToHashSet(), Size);
+        
+        public World? Stepped()
+        {
+            var cloned = Cloned();
+        
+            return cloned.Step() is {isValid: true} ? cloned : null;
+        }
+
+        private Guard? NextGuard()
+        {
+            var direction = Guard.Direction;
+            var maybeNewPosition = Guard.Position.Moved(direction);
+            
+            while (Obstructions.Contains(maybeNewPosition))
+            {
+                direction = direction.RotatedRight();
+                maybeNewPosition = Guard.Position.Moved(direction);
+            }
+
+            if (!IsInGrid(maybeNewPosition))
+            {
+                return null;
+            }
+            
+            return new Guard(maybeNewPosition, direction);
+        }
+        
+        private bool IsInGrid(Vec2 position)
+        {
+            return position.C >= 0 && position.R >= 0 && position.C < Size.C && position.R < Size.R;
         }
     }
-
-    private record Obstruction(Vec2 Position) : IGridItem;
-
+    
     protected override long Part1(string input)
     {
         var charGrid = ParseInput(input);
-
-        // var gridDict = new Dictionary<(int x, int y), IGridItem>();
-        var obstructions = LocateObstructions(charGrid).Select(o => o.Position).ToFrozenSet();
+        var obstructions = LocateObstructions(charGrid).ToFrozenSet();
 
         var guard = LocateGuard(charGrid);
-        
-        var isInGrid = true;
 
-        HashSet<Vec2> visited = [guard.Position];
+        var world = new World(guard, obstructions, new Vec2(charGrid.Length, charGrid[0].Length));
         
-        while (isInGrid)
+        while (world.Stepped() is {} nextWorld)
         {
-            guard.Move(obstructions);
-            
-            isInGrid = guard.Position.C < charGrid[0].Length && guard.Position.R < charGrid.Length;
-
-            if (isInGrid)
-            {
-                visited.Add(guard.Position);
-            }
+            world = nextWorld;
         }
-
-        return visited.Count;
+        
+        return world.Visited.Select(g => g.Position).Distinct().Count();
     }
 
-    private static Obstruction[] LocateObstructions(char[][] grid)
+    protected override long Part2(string input)
     {
-        List<Obstruction> obstructions = [];
+        var charGrid = ParseInput(input);
+        var obstructions = LocateObstructions(charGrid).ToFrozenSet();
+
+        var guard = LocateGuard(charGrid);
+
+        var worldSize = new Vec2(charGrid.Length, charGrid[0].Length);
+        
+        var world = new World(guard, obstructions, worldSize);
+        
+        List<World> worlds = [];
+        
+        while (world.Stepped() is {} nextWorld)
+        {
+            world = nextWorld;
+            worlds.Add(nextWorld);
+        }
+
+        var potentialObstructions = worlds.Select(w => w.Guard.Position).Distinct().ToFrozenSet();
+
+        var possibleObstructions = new HashSet<Vec2>();
+        
+        foreach (var potentialObstruction in potentialObstructions)
+        {
+            world = new World(guard, [..obstructions, potentialObstruction], worldSize);
+
+            if (world.WillLoop())
+            {
+                possibleObstructions.Add(potentialObstruction);
+            }
+        }
+        
+        Console.WriteLine($"Number of potential obstructions: {potentialObstructions.Count}");
+        Console.WriteLine($"Number of possible obstructions: {possibleObstructions.Count}");
+
+        return possibleObstructions.Count;
+    }
+
+    private static Vec2[] LocateObstructions(char[][] grid)
+    {
+        List<Vec2> obstructions = [];
 
         for (var r = 0; r < grid.Length; r++)
         {
@@ -134,7 +150,7 @@ public class Day6() : AoCDay(day: 6, hasTwoInputs: false)
             {
                 if (row[c] == '#')
                 {
-                    obstructions.Add(new Obstruction(new Vec2(r, c)));
+                    obstructions.Add(new Vec2(r, c));
                 }
             }
         }
@@ -150,9 +166,9 @@ public class Day6() : AoCDay(day: 6, hasTwoInputs: false)
 
             for (int c = 0; c < row.Length; c++)
             {
-                if (_guardSymbols.Contains(row[c]))
+                if (Directions.All.Contains(row[c]))
                 {
-                    return new Guard(new Vec2(r, c), ParseDirection(row[c]));
+                    return new Guard(new Vec2(r, c), row[c]);
                 }
             }
         }
@@ -166,111 +182,7 @@ public class Day6() : AoCDay(day: 6, hasTwoInputs: false)
 
         return lines.Select(l => l.ToCharArray()).ToArray();
     }
-
-    protected override long Part2(string input)
-    {
-        var charGrid = ParseInput(input);
-
-        // var gridDict = new Dictionary<(int x, int y), IGridItem>();
-        var obstructions = LocateObstructions(charGrid).Select(o => o.Position).ToFrozenSet();
-
-        var guard = LocateGuard(charGrid);
-        
-        var isInGrid = true;
-
-        Dictionary<Vec2, Orientation> visited = [];
-        visited.Add(guard.Position, guard.Direction);
-        
-        var path = new Stack<(Vec2 Position, Orientation Direction)>();
-        
-        while (isInGrid)
-        {
-            guard.Move(obstructions);
-            
-            isInGrid = guard.Position.C < charGrid[0].Length && guard.Position.R < charGrid.Length;
-
-            if (isInGrid)
-            {
-                if (visited.TryGetValue(guard.Position, out var prevDirections))
-                {
-                    visited[guard.Position] |= guard.Direction;
-                }
-                else
-                {
-                    visited.Add(guard.Position, guard.Direction);
-                }
-                
-                path.Push((guard.Position, guard.Direction));
-            }
-        }
-
-        Console.WriteLine($"{visited.Count}; {path.Count}; {path.Peek().Position}");
-
-        var options = new HashSet<Vec2>();
-        
-        foreach (var (position, direction) in path)
-        {
-            var nextPosition = position.MoveUnit(direction);
-            
-            // There is already an obstruction
-            if (obstructions.Contains(nextPosition)) continue;
-            
-            var nextPositionWhenBlocked = position.MoveUnit(TurnRight(direction));
-            
-            if (visited.ContainsKey(nextPositionWhenBlocked) &&
-                visited[nextPositionWhenBlocked] == TurnRight(direction))
-            {
-                options.Add(nextPosition);
-            }
-        }
-        
-        Console.WriteLine("options:");
-        Console.WriteLine(string.Join("\n", options));
-
-        char[][] newGrid = charGrid.Select(r => r.Select(i => i).ToArray()).ToArray();
-
-        foreach (var (p, o) in visited)
-        {
-            newGrid[p.R][p.C] = ToChar(o);
-        }
-
-        foreach (var option in options)
-        {
-            char[][] optionGrid = newGrid.Select(r => r.Select(i => i).ToArray()).ToArray();
-            
-            optionGrid[option.R][option.C] = 'O';
-            
-            Console.WriteLine($"\nAn option ({option}):");
-            PrintGrid(optionGrid);
-        }
-        
-
-        return options.Count;
-    }
-
-    private void PrintGrid(char[][] newGrid)
-    {
-        for (int r = 0; r < newGrid.Length; r++)
-        {
-            var row = newGrid[r];
-            Console.WriteLine(new string(row));
-        }
-    }
-
-    private char ToChar(Orientation orientation)
-    {
-        if (orientation.HasFlag(Orientation.Up) || orientation.HasFlag(Orientation.Down))
-        {
-            if (orientation.HasFlag(Orientation.Left) || orientation.HasFlag(Orientation.Right))
-            {
-                return '+';
-            }
-
-            return '|';
-        }
-
-        return '-';
-    }
+    
 
     [Test]
     [TestCase(1, 41)]
@@ -285,5 +197,52 @@ public class Day6() : AoCDay(day: 6, hasTwoInputs: false)
         };
 
         Assert.That(result, Is.EqualTo(expected));
+    }
+}
+
+public readonly record struct Vec2(int R, int C)
+{
+    public Vec2 Moved(Vec2 v)
+    {
+        return new Vec2(R + v.R, C + v.C);
+    }
+        
+    public Vec2 Moved(char direction)
+    {
+        return Moved(direction.AsUnit());
+    }
+}
+
+public static class Directions
+{
+    public const char Up = '^';
+    public const char Down = 'v';
+    public const char Right = '>';
+    public const char Left = '<';
+        
+    public static readonly char[] All = [Up, Right, Down, Left, Up];
+        
+    public static Vec2 AsUnit(this char direction)
+    {
+        return direction switch
+        { 
+            Up => new Vec2(-1, 0),
+            Down => new Vec2(1, 0),
+            Left => new Vec2(0, -1),
+            Right => new Vec2(0, 1),
+            _ => throw new ArgumentOutOfRangeException(nameof(direction), direction, null)
+        };
+    }
+    
+    public static char RotatedRight(this char direction)
+    {
+        return direction switch
+        {
+            Up => Right,
+            Right => Down,
+            Down => Left,
+            Left => Up,
+            _ => throw new ArgumentOutOfRangeException(nameof(direction), direction, null)
+        };
     }
 }
